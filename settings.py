051939825176d338e6fff17529997dd8f4b4c2e4
Joshua Bonnett <jbonnett@ThinkPad-X61s.(none)>
2011-05-06 10:29:00 -0700

    genralized the settings

--- settings.py
+++ settings.py
@@ -126,7 +126,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
-    
+    'armstrong.core.arm_content',
     'armstrong.core.arm_wells',
     'reversion',
     'chunks',
