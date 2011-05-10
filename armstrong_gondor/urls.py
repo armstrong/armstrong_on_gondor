514d03e1eb1390cfa00a5304ea5fad9070677f1d
Joshua Bonnett <jbonnett@baycitizen.org>
2011-05-09 18:13:23 -0700

    set up the index as a static template

--- armstrong_gondor/urls.py
+++ armstrong_gondor/urls.py
@@ -10,8 +10,9 @@ import armstrong.apps.arm_events.urls as event_urls
 
 urlpatterns = patterns('',
     url(r'^contact/', include('contact_form.urls')),
-    url(r'^$', SimpleWellView.as_view(template_name='home/index.html',
-                                      well_title='Generic'), name='home'),
+    url(r'^$','django.views.generic.simple.direct_to_template',{'template':'index.html'}),
+    url(r'^well/$', SimpleWellView.as_view(template_name='home/index.html',
+                                      well_title='Generic'), name='welltest'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
